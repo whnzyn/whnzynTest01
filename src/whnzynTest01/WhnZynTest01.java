e3a37cf76ac361b2926ff537069a6f275ad91bf9
wanghao <whnzyn@qq.com>
2016-10-09 08:24:42 +0800

    3333333333

--- src/whnzynTest01/WhnZynTest01.java
+++ src/whnzynTest01/WhnZynTest01.java
@@ -4,5 +4,6 @@ public class WhnZynTest01 {
 	public static void main(String[] args) {
 		System.out.println("11111111111111");
 		System.out.println("2222222222222");
+		System.out.println("333333333333");
 	}
 }
